9003f7c9f6978c7cc05415d0987653f6624f3a67
goemon024 <hirotorics@gmail.com>
2025-05-27 14:41:29 +0900

    modifying pagination

--- app/components/pagination/pagination.tsx
+++ app/components/pagination/pagination.tsx
@@ -22,67 +22,73 @@ export const Pagination = ({
   const searchParams = useSearchParams();
   const [isLoading, setIsLoading] = useState(false);
 
-  const goToPage = async (page: number) => {
-    setIsLoading(true)
-    try {
-      const params = new URLSearchParams(searchParams.toString());
-      params.set("page", page.toString());
-      if (keyword) params.set("keyword", keyword);
-      await router.push(`/home?${params.toString()}`);
-    } catch (error) {
-      console.error("ページ遷移に失敗しました:", error);
-    } finally {
-      setIsLoading(false);
-    }
+  const goToPage = (page: number) => {
+    setIsLoading(true);
+    setTimeout(async () => {
+      try {
+        const params = new URLSearchParams(searchParams.toString());
+        params.set("page", page.toString());
+        if (keyword) params.set("keyword", keyword);
+        await router.push(`/home?${params.toString()}`);
+      } catch (error) {
+        console.error("ページ遷移に失敗しました:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    }, 30);
   };
 
   if (totalPages <= 1) return null;
 
   return (
-    <div className={styles.pagination}>
-      <button
-        className={
-          currentPage === 1
-            ? styles.disabledPreviousButton
-            : styles.previousButton
-        }
-        onClick={() => goToPage(currentPage - 1)}
-        disabled={currentPage === 1}
-      >
-        <AiOutlineArrowLeft className={styles.arrowIcon} />
-        <span className={styles.spanStyle}></span>
-        Previous Page
-      </button>
+    <>
+      {isLoading && <LoadingModal />}
 
-      <div className={styles.buttonsContainer}>
-        {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
-          <button
-            key={page}
-            className={
-              page === currentPage ? styles.currentButton : styles.pagiButton
-            }
-            onClick={() => goToPage(page)}
-            style={{ fontWeight: page === currentPage ? "bold" : "normal" }}
-          >
-            {page}
-          </button>
-        ))}
+      <div className={styles.pagination}>
+        <button
+          className={
+            currentPage === 1
+              ? styles.disabledPreviousButton
+              : styles.previousButton
+          }
+          onClick={() => goToPage(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          <AiOutlineArrowLeft className={styles.arrowIcon} />
+          <span className={styles.spanStyle}></span>
+          Previous Page
+        </button>
+
+        <div className={styles.buttonsContainer}>
+          {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
+            <button
+              key={page}
+              className={
+                page === currentPage ? styles.currentButton : styles.pagiButton
+              }
+              onClick={() => goToPage(page)}
+              style={{ fontWeight: page === currentPage ? "bold" : "normal" }}
+            >
+              {page}
+            </button>
+          ))}
+        </div>
+
+        <button
+          className={
+            currentPage === totalPages
+              ? styles.disabledNextButton
+              : styles.nextButton
+          }
+          onClick={() => goToPage(currentPage + 1)}
+          disabled={currentPage === totalPages}
+          // style={{ background: "inherit" }}
+        >
+          Next Page
+          <span className={styles.spanStyle}></span>
+          <AiOutlineArrowRight className={styles.arrowIcon} />
+        </button>
       </div>
-
-      <button
-        className={
-          currentPage === totalPages
-            ? styles.disabledNextButton
-            : styles.nextButton
-        }
-        onClick={() => goToPage(currentPage + 1)}
-        disabled={currentPage === totalPages}
-      // style={{ background: "inherit" }}
-      >
-        Next Page
-        <span className={styles.spanStyle}></span>
-        <AiOutlineArrowRight className={styles.arrowIcon} />
-      </button>
-    </div>
+    </>
   );
 };
